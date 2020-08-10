512c2c7d091e74d360ab0c084ffa2069f1428558
anpeibooa <56881476+anpeibooa@users.noreply.github.com>
2020-08-10 16:03:20 -0400

    Update cutfunctionpy.py
    fixed a glitch which causes the function to overwrite the input array

--- cutfunctionpy.py
+++ cutfunctionpy.py
@@ -1,20 +1,21 @@
 import numpy as np
 
+
 def cut(xx,threshold,E):
     mysum=[]
     mymul=[]
     myvar=[]
     index1=[]
-    x=xx
     
+    x=np.zeros((xx.shape[0],xx.shape[1]))
     
     for i in range(0,x.shape[0]):
 
         
-        Tobesum=np.where(x[i][:(x.shape[1]-1)] < threshold, 0, x[i][:(x.shape[1]-1)])
-        x[i][:(x.shape[1]-1)]=np.where(x[i][:(x.shape[1]-1)] < threshold, 0, x[i][:(x.shape[1]-1)])
-        compare=Tobesum > 55
-        localsum=np.sum(Tobesum)
+        x[i][:(xx.shape[1]-1)]=np.where(xx[i][:(xx.shape[1]-1)] < threshold, 0, xx[i][:(xx.shape[1]-1)])
+        x[i][xx.shape[1]-1]=xx[i][xx.shape[1]-1]
+        compare=x[i][:(xx.shape[1]-1)] > 55
+        localsum=np.sum(x[i][:(xx.shape[1]-1)])
         if True in compare or localsum < E:
             index1.append(i)
             
@@ -24,7 +25,7 @@ def cut(xx,threshold,E):
         else:
             
             mysum.append(localsum)        
-            mymul.append(np.count_nonzero(Tobesum))
-            myvar.append((np.std(Tobesum))**2.0)
+            mymul.append(np.count_nonzero(x[i][:(xx.shape[1]-1)]))
+            myvar.append((np.std(x[i][:(xx.shape[1]-1)]))**2.0)
     newx=np.delete(x,index1,axis=0)
     return (mysum,mymul,newx,myvar)
